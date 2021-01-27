5a95daed7650280317ea3b57cfecfa89099dac30
Sergey <cabyrkih@mail.ru>
2021-01-27 13:04:11 +0300

    js scroll

--- js/script.js
+++ js/script.js
@@ -22,6 +22,20 @@ product.forEach(function (event) {
 })
 
 
+let anchors = document.querySelectorAll('a[href*="#"')
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (event) {
+		event.preventDefault()
+		let blockId = anchor.getAttribute('href')
+
+		document.querySelector(blockId).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+
+	})
+}
 
 /* active(onclick) */
 
